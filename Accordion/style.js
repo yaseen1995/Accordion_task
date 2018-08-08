03d95cb45a101f5b11c99d07bc14400a73cf2020
yaseen <p14159245@my365.dmu.ac.uk>
2018-08-08 16:02:43 +0100

    Full project
    Contains the front end and integration with the JSON data displayed on
    the interface

---
+++ Accordion/style.js
@@ -0,0 +1,18 @@
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function () {
+    this.classList.toggle('open');
+    var content = this.nextElementSibling;
+
+    if(content.style.maxHeight) {
+
+      content.style.maxHeight = null;
+    }
+
+    else {
+
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
